40cedff9a44118b418c279ee0c784a490a7e12ef
Renata Pires <renata.pires@indicium.tech>
2024-02-26 16:52:57 +0000

    fix.2

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,43 +1,42 @@
+-- models/marts/dim_customers.sql
+
 WITH stg_customer AS (
     SELECT 
         customerid,
         personid,
         storeid
-    FROM {{ ref('stg_customer') }}
+    FROM `adventureworksdesafiolh`.`dbt_rpires`.`stg_customer`
 ),
 
 stg_person AS (
     SELECT
         businessentityid,
-        -- Concatenando primeiros, meios e sobrenomes
         CONCAT(IFNULL(firstname, ''), ' ', IFNULL(middlename, ''), ' ', IFNULL(lastname, '')) AS fullname
-    FROM {{ ref('stg_person') }}
+    FROM `adventureworksdesafiolh`.`dbt_rpires`.`stg_person`
 ),
 
 stg_store AS (
     SELECT
         businessentityid AS storebusinessentityid,
-        storename
-    FROM {{ ref('stg_store') }}
+        storename -- Certifique-se de que esta é a coluna correta
+    FROM `adventureworksdesafiolh`.`dbt_rpires`.`stg_store`
 ),
 
 transformed AS (
     SELECT
-        ROW_NUMBER() OVER (ORDER BY stg_customer.customerid) AS customer_sk, -- Chave substituta autoincremental
+        ROW_NUMBER() OVER (ORDER BY stg_customer.customerid) AS customer_sk,
         stg_customer.customerid,
         stg_person.businessentityid,
         stg_person.fullname,
         stg_store.storebusinessentityid,
         stg_store.storename,
         CASE
-            -- Para cada customerid, quando não há nome de cliente associado, um nome de loja é definido
             WHEN stg_person.fullname IS NULL THEN stg_store.storename
             ELSE stg_person.fullname 
         END AS customer_fullname
     FROM stg_customer
     LEFT JOIN stg_person ON stg_customer.personid = stg_person.businessentityid
-    LEFT JOIN stg_store ON stg_customer.storeid = stg_store.storebusinessentityid
-)
+    LEFT JOIN stg_store ON stg_customer.storeid = stg_store
 
-SELECT *
-FROM transformed
+
+
--- models/staging/stg_countryregion.sql
+++ models/staging/stg_countryregion.sql
@@ -1,8 +1,11 @@
-with source_data as 
+-- models/staging/stg_countryregion.sql
+
+create or replace view `adventureworksdesafiolh`.`dbt_rpires`.`stg_countryregion`
+OPTIONS()
+as (
     select
-        countryregioncode
-        , modifieddate
-        , name as country_name
+        countryregioncode,
+        modifieddate,
+        name as country_name
     from `adventureworksdesafiolh.dbt_rpires.countryregion`
-select *
-from source_data
+);
