790624048ddc0819da0f5c23a2ccbf83048d2043
Renata Pires <renata.pires@indicium.tech>
2024-02-27 21:15:10 +0000

    fact_agregate_table

--- models/marts/fact_agregate_daily_sales.sql
+++ models/marts/fact_agregate_daily_sales.sql
@@ -3,17 +3,22 @@
     schema='aggregated_tables'
 ) }}
 
-with sales_data as (
+with salesperson_region_sales as (
     select
-        region_id,
-        salesperson_id,
-        sum(unitprice * orderqty) as total_sales
-    from {{ ref('fact_sales') }}
-    group by region_id, salesperson_id
+        s.salesperson_id,
+        l.region_id,
+        sum(f.unitprice * f.orderqty) as total_sales
+    from {{ ref('fact_sales') }} f
+    join {{ ref('stg_salesorderheader') }} s on f.salesperson_id = s.salesperson_id
+    join {{ ref('dim_locations') }} l on f.location_id = l.location_id
+    group by s.salesperson_id, l.region_id
 )
 
 select
-    region_id,
     salesperson_id,
+    region_id,
     total_sales
-from sales_data
+from salesperson_region_sales
+order by salesperson_id, region_id
+
+
