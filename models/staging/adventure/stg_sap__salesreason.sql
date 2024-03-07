042197bfe15dd8ac37e817dfb405920a46b6ca0e
renata.pires@indicium.tech <renata.pires@indicium.tech>
2024-03-07 13:52:22 +0000

    new version

--- models/staging/adventure/stg_sap__salesreason.sql
+++ models/staging/adventure/stg_sap__salesreason.sql
@@ -1,11 +1,10 @@
-with
-    fonte_salesreason as (
-        select 
-        cast(salesreasonid as int) as id_motivo
-        , cast(name as string) as nome_motivo
-        , cast(reasontype as string) as nome_razao_motivo
-       -- , cast(modifieddate as tipo) as nome
-        from {{ source('sap', 'salesreason') }}
-    )
+with fonte_salesreason as (
+    select 
+        cast(salesreasonid as int) as id_motivo,
+        cast(name as string) as nome_motivo,
+        cast(reasontype as string) as nome_razao_motivo
+    from {{ source('sap', 'salesreason') }}
+)
+
 select *
 from fonte_salesreason
